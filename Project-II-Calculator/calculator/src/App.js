4a633dc613a89a1d5f34c6c0a088b545fa7a5db3
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-11 21:11:57 +0100

    style: Style buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
-import NumberButton from "./components/ButtonComponents/NumberButton";
+import Button from "./components/ButtonComponents/Button";
+
 const App = () => {
 	return (
 		<div className="environmentContainer">
 			<div className="calculatorContainer">
 				<CalculatorDisplay value={0} />
+				<Button />
 			</div>
 		</div>
 	);
