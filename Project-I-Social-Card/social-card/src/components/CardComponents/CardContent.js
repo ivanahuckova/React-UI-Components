c9f918cee56030b1f26f93eb0b1a8c9537793974
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-11 16:13:01 +0100

    fead: Add card content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,12 +3,13 @@ import "./Card.css";
 
 export default function CardContent() {
 	return (
-		<div className>
-			<div>
-				<h3>Get Started with React</h3>
+		<div className="CardContentContainer">
+			<h3>Get Started with React</h3>
+
+			<div className="CardContentContent">
+				React makes it painless to create interactive UIs. Design simple views for each state in your application
 			</div>
-			<div>React makes it painless to create interactive UIs. Design simple views for each state in your application</div>
-			<div>reactjs.org</div>
+			<div className="CardContentLink">reactjs.org</div>
 		</div>
 	);
 }
