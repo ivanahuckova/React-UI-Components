27ab4e73b3fe67fea587b0d5509dfcf1b68120dd
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-11 16:42:27 +0100

    feat: Add calculator display

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,6 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+export default function ActionButton() {
+	return <div />;
+}
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+export default function CalculatorDisplay() {
+	return (
+		<div className="displayContainer">
+			<div className="displayResult">0</div>
+		</div>
+	);
+}
