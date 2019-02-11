d77e4597ef1d0190e404a610ef0f947461bf907d
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-11 15:39:59 +0100

    feat: Add header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail.js";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+export default function HeaderContainer() {
+	return (
+		<div className="headerMainContainer">
+			<ImageThumbnail />
+			<div className="rightHeaderContainer">
+				<HeaderTitle />
+				<HeaderContent />
+			</div>
+		</div>
+	);
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export default function HeaderTitle() {
+	return (
+		<div className="headerTitleContainer">
+			<div className="headerTitle">Lambda School</div>
+			<div className="headerUsername">@LambdaSchool</div>
+			<ul className="headerDateUl">
+				<li className="headerDateLi">26 jan</li>
+			</ul>
+		</div>
+	);
+}
