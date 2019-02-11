2ecea44f4a7299f69389bbe5056c94f027de9592
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-11 21:05:13 +0100

    style: Styling buttons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const ActionButton = ({ value }) => {
-	return <button className="action-button">{value}</button>;
+	return <button className="actionButton">{value}</button>;
 };
 
 export default ActionButton;
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Button.css";
+import NumberButton from "./NumberButton";
+import ActionButton from "./ActionButton";
+
+const Button = () => {
+	<div className="mainButtonContainer">
+		<ActionButton value="clear" />
+		<NumberButton buttonStyle="operationsButton" text="÷" />
+		<NumberButton buttonStyle="numberButton" text="7" />
+		<NumberButton buttonStyle="numberButton" text="8" />
+		<NumberButton buttonStyle="numberButton" text="9" />
+		<NumberButton buttonStyle="operationsButton" text="x" />
+		<NumberButton buttonStyle="numberButton" text="4" />
+		<NumberButton buttonStyle="numberButton" text="5" />
+		<NumberButton buttonStyle="numberButton" text="6" />
+		<NumberButton buttonStyle="operationsButton" text="-" />
+		<NumberButton buttonStyle="numberButton" text="1" />
+		<NumberButton buttonStyle="numberButton" text="2" />
+		<NumberButton buttonStyle="numberButton" text="3" />
+		<NumberButton buttonStyle="operationsButton" text="+" />
+		<ActionButton value="0" />
+		<NumberButton buttonStyle="operationsButton" text="=" />
+	</div>;
+};
+
+export default Button;
